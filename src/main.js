a62c67b81d89c101d5281846a751b5f499d7faf0
thang.cao <thang.cao@9thwonder.com>
2022-12-01 11:18:30 +0700

    add form login

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import './style.css'
 import App from './App.vue'
 import Home from './pages/Home.vue'
 import ClickGame from './pages/ClickGame.vue'
+import Form from './pages/Form.vue'
 
 const app = createApp(App)
 
 const routes = [
   { path: '/', component: Home },
   { path: '/click-game', component: ClickGame },
+  { path: '/form', component: Form },
 ]
 const router = createRouter({
   // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
---
+++ src/pages/Form.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+
+const formData = ref({})
+const onSubmit = () => {
+  alert(`
+    User Name: ${formData.value.userName || ''}
+    Password: ${formData.value.password || ''}
+  `)
+}
+</script>
+<template>
+  <div class="h-screen flex flex-col items-center justify-center">
+    <form class="p-10 rounded-md border-4 border-gray-700" @submit="onSubmit">
+      <h1 class="mb-5 text-3xl">Login</h1>
+      <div>
+        <label for="userName"></label>
+        <input id="userName" v-model="formData.userName" type="text" class="border-2 w-[500px] p-2 rounded-sm" placeholder="User Name">
+      </div>
+      <div class="mt-2">
+        <label for="password"></label>
+        <input id="password" v-model="formData.password" type="password" class="border-2 w-[500px] p-2 rounded-sm" placeholder="Password">
+      </div>
+      <div class="text-right mt-5">
+        <button class="py-2 px-10 rounded-md bg-blue-400 text-white">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
