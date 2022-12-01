36567edd5a4fbe2eb0d7d077be37896ae63322f8
thang.cao <thang.cao@9thwonder.com>
2022-12-01 10:56:34 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import './style.css'
+import App from './App.vue'
+import Home from './pages/Home.vue'
+
+const app = createApp(App)
+
+const routes = [
+  { path: '/', component: Home },
+]
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+})
+
+app.use(router)
+
+app.mount('#app')
+
---
+++ src/pages/ClickGame.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>Timer: 0</div>
+    <div>0</div>
+    <div>
+      <button>Start</button>
+    </div>
+    <div>
+      <button>Click me</button>
+    </div>
+    <div>
+      <button>Reset</button>
+    </div>
+  </div>
+</template>
