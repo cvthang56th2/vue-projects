646167f385236c4beae4851e70675172e03f50e6
thang.cao <thang.cao@9thwonder.com>
2022-12-07 11:25:13 +0700

    update UI

--- src/pages/StopWatch.vue
+++ src/pages/StopWatch.vue
@@ -1,7 +1,29 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
+const running = ref(false)
 const mode = ref('stopwatch')
+const stopTime = ref(0)
+const stopData = computed(() => {
+  const centiseconds = parseInt(stopTime.value / 10)
+  const seconds = parseInt(stopTime.value / 100)
+  const minutes = parseInt(seconds / 60)
+  return {
+    centiseconds,
+    seconds,
+    minutes
+  }
+})
+let interval
+const toggleStart = () => {
+  running.value = !running.value
+  clearInterval(interval)
+  if (running.value) {
+    interval = setInterval(() => {
+      stopTime.value++
+    }, 10);
+  }
+}
 </script>
 <template>
   <div class="h-screen flex flex-col items-center justify-center">
@@ -10,8 +32,8 @@ const mode = ref('stopwatch')
       <button class=" ml-10" :class="mode === 'timer' ? 'text-3xl' : 'text-xl'" @click="mode = 'timer'">Timer</button>
     </div>
     <div class="my-10">
-      <div v-if="mode === 'stopwatch'">
-        Watch
+      <div v-if="mode === 'stopwatch'" class="rounded-full text-4xl">
+        {{ stopData.minutes || '00' }}:{{ stopData.seconds || '00' }}:{{ stopData.centiseconds || '00' }}
       </div>
       <div v-else>
         H M S
@@ -19,7 +41,7 @@ const mode = ref('stopwatch')
     </div>
     <div class="flex">
       <button class="rounded-md py-2 w-[150px] border-2">Reset</button>
-      <button class="rounded-md py-2 w-[150px] border-2 ml-20">Start</button>
+      <button class="rounded-md py-2 w-[150px] border-2 ml-20" @click="toggleStart">{{ running ? 'Stop' : 'Start' }}</button>
     </div>
   </div>
 </template>
